4f42e8ca1726c48aa38db032039b7e08ebdae2d6
mi-hye <satge13@gmail.com>
2024-03-30 16:30:55 +0900

    refactor: pageChangeFunc code refactoring

--- js/buttonAction.js
+++ js/buttonAction.js
@@ -2,7 +2,15 @@ const [$prevBtn, $nextBtn] = document.querySelectorAll(".button-wrap button");
 const $contentWrap = document.querySelector(".cd-wrap");
 const $diskInners = document.querySelectorAll(".disk_inner");
 const $albums = document.querySelectorAll(".album");
-// const bgArray ;
+const bgArray = [
+	["#0272a4", "#f6a564"],
+	["#b6bfc8", "#36595b"],
+	["#e58e82", "#6f569f"],
+	["#d4b386", "#d49a7b"],
+	["#b13134", "#4f2107"],
+	["#dddfdc", "#77948a"],
+];
+
 function readyButton() {
 	const totalNum = $albums.length;
 	let pageNum = 0;
@@ -23,10 +31,11 @@ function readyButton() {
 }
 
 function pageChangeFunc(pageNum, totalNum) {
+	$contentWrap.style.background = `linear-gradient(120deg,${bgArray[pageNum][0]},${bgArray[pageNum][1]})`;
 	Array.from({ length: totalNum }).forEach((_, i) => {
 		$albums[i].classList.remove("active");
-		if (pageNum === i) $albums[i].classList.add("active");
 	});
+	$albums[pageNum].classList.add("active"); // 현재 페이지의 album을 활성화
 }
 
 document.addEventListener("DOMContentLoaded", readyButton);
